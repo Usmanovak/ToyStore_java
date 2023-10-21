042853c7f374b500cbfbbadc452fc490962ecdac
Karina <usmkarin@gmail.com>
2023-10-21 21:17:34 +0700

    done

---
+++ presenters/Outinter.java
@@ -0,0 +1,6 @@
+package presenters;
+import models.Toy;
+
+public interface Outinter {
+    void listOfPresents(Toy toy);
+}
---
+++ presenters/Views.java
@@ -0,0 +1,8 @@
+package presenters;
+import models.Toy;
+import java.util.List;
+
+public interface Views {
+    void showShowcase(List<Toy> toyArray);
+    void ShowFortuneRezult(Toy toy);
+}
