0699af5eaf207f5c83d92be43b200b4697af9a0f
Karina <usmkarin@gmail.com>
2023-10-21 21:17:20 +0700

    done

---
+++ models/ShowCase.java
@@ -0,0 +1,11 @@
+package models;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowCase {
+    private final List<Toy> mainArray=new ArrayList<>();
+
+    public List<Toy> getMainArray() {
+        return mainArray;
+    }
+}
